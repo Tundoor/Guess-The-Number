1d3ae9eb68aa641d2c05d22b7d5117816ebb21a1
Thando <thandwem045@gmail.com>
2025-04-30 20:40:54 +0200

    Added css for user input

---
+++ index.js
@@ -0,0 +1,7 @@
+const score = document.getElementById("score");
+const rules = document.getElementById("Rules-div");
+const gamesDiv = document.getElementById("Game-div");
+const guessBtn = document.getElementById("Input");
+const modal = document.getElementById("Modal");
+const done = document.getElementById("done");
+
